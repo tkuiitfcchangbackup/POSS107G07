6a8b2385d3a88462b4285350ef372b56b2810380
dolala9875 <dolala9875@gmail.com>
2019-05-14 13:20:27 +0000

    p

--- WaterCarrier.php
+++ WaterCarrier.php
@@ -1,7 +1,26 @@
 <link rel="stylesheet" type="text/css" href="style.css">
 
 <html>
+    <head>
+          <title>
+                WaterCarrier
+          </title>
+    </head>
     <h2>
          Water Carrier
     </h2>
+    
+         <blockquote><blockquote><blockquote><blockquote><blockquote><blockquote>
+            <table style="position:relative; top:80px; border-top:4px #FFD382 solid;border-bottom:4px #FFD382 solid;border-right:4px #FFD382 solid; border-left:4px #FFD382 solid;" cellpadding="15" border='0'>
+
+         <tr><td><a href="/POSS107G07/main.php">HOME</a></td></tr>
+	 <tr><td><a href="/POSS107G07/Fish.php">Fish(2/19-3/20)</a></td></tr>
+	 <tr><td><a href="/POSS107G07/Ram.php">Ram(3/21-4/20)</a></td></tr>
+	 <tr><td><a href="/POSS107G07/Bull.php">Bull(4/21-5/20)</a></td></tr>
+	 <tr><td><a href="/POSS107G07/Twins.php">Twins(5/21-6/21)</a></td></tr>
+         </blockquote></blockquote></blockquote></blockquote></blockquote></blockquote>
+        <body>
+	水瓶座的人頗富知性，且具有銳利的觀察力
+      
+    </body>   
 </html>
