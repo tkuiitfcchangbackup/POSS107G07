6d93ec1828fca4e9018e4dacf89038b301df2f4e
dolala9875 <dolala9875@gmail.com>
2019-05-23 07:29:50 +0000

    p

--- main.php
+++ main.php
@@ -9,25 +9,43 @@
 		        Constellation
 	       </h1>
 	       <hr sixe="30px" align="center" width="100%">
-	<body>
+	<body background ="/POSS107G07/bg1.jpg">
 	      <blockquote><blockquote><blockquote><blockquote><blockquote><blockquote>
                <div style = "float:left">
 		<table style="position:relative; top:60px; border-top:4px #FFD382 solid;border-bottom:4px #FFD382 solid;border-right:4px #FFD382 solid;border-left:4px #FFD382 solid;" cellpadding="15" border='0'>
 	
 
 
-		<tr><td align="center"><a href="/POSS107G07/WaterCarrier.php"><img src="/POSS107G07/watercarrier.png" alt="HTML tutorial" style = "width:100px;height:100px;border:0;"></a></td>
+	<tr>
+		<td align="center"><a href="/POSS107G07/WaterCarrier.php"><img src="/POSS107G07/watercarrier.png" alt="HTML tutorial" style = "width:100px;height:100px;border:0;"></a></td>
+
 		<td><a href="/POSS107G07/Fish.php">Fish(2/19-3/20)</a></td>
+		
 		<td align="center"><a href="/POSS107G07/Ram.php"><img src="/POSS107G07/ram.png" alt="HTML tutorial" style = "width:100px;height:100px;border:0;" ></a></td>
-		<td><a href="/POSS107G07/Bull.php">Bull(4/21-5/20)</a></td></tr>
-		<tr><td><a href="/POSS107G07/Twins.php">Twins(5/21-6/21)</a></td></tr>
-		<tr><td><a href="/POSS107G07/Crab.php">Crab(6/22-7/22)</a></td></tr>
-		<tr><td><a href="/POSS107G07/Lion.php">Lion(7/23-8/22)</a></td></tr>
-		<tr><td><a href="/POSS107G07/Virgin.php">Virgin(8/23-9/22)</a></td></tr>
-		<tr><td><a href="/POSS107G07/Scales.php">Scales(9/23-10/23)</a></td></tr>
-		<tr><td><a href="/POSS107G07/Scoipion.php">Scoipion(10/24-11/22)</a></td></tr>
-		<tr><td><a href="/POSS107G07/Archer.php">Archer(11/23-12/21)</a></td></tr>
-		<tr><td><a href="/POSS107G07/Goat.php">Goat(12/22-1/20)</a></td></tr>
+		
+		<td align="center"><a href="/POSS107G07/Bull.php"><img src="/POSS107G07/bull.png" alt="HTML tutorial" style = "width:100px;height:100px;border:0;"></a></td>
+	</tr>
+		
+	<tr>
+		<td><a href="/POSS107G07/Twins.php">Twins(5/21-6/21)</a></td>
+		
+		<td><a href="/POSS107G07/Crab.php">Crab(6/22-7/22)</a></td>
+		
+		<td><a href="/POSS107G07/Lion.php">Lion(7/23-8/22)</a></td>
+		
+		<td><a href="/POSS107G07/Virgin.php">Virgin(8/23-9/22)</a></td>
+	</tr>
+		
+	<tr>
+		<td><a href="/POSS107G07/Scales.php">Scales(9/23-10/23)</a></td>
+		
+		<td><a href="/POSS107G07/Scoipion.php">Scoipion(10/24-11/22)</a></td>
+		
+		<td><a href="/POSS107G07/Archer.php">Archer(11/23-12/21)</a></td>
+		
+		<td><a href="/POSS107G07/Goat.php">Goat(12/22-1/20)</a></td>
+	</tr>
+
              </blockquote></blockquote></blockquote></blockquote></blockquote></blockquote>
 </table>
 </div>
